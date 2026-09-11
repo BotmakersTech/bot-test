7b3705633aebb185ed341172241dfe9323ed849b
pallavid18 <pallavidhope9@gmail.com>
2026-09-11 18:57:28 +0530

    Made changes in padding and font size and alignment

--- Botleague-Frontend/src/temp/pages/ContactUs.tsx
+++ Botleague-Frontend/src/temp/pages/ContactUs.tsx
@@ -1,10 +1,12 @@
 import Hero from "./contact/Hero";
 import ContactSection from "./contact/ContactSection";
 import OfficeSection from "./contact/OfficeSection";
+import PublicNavbar from "../../shared/components/PublicNavbar";
 
 export default function ContactUs() {
   return (
     <>
+    <PublicNavbar/>
       <Hero />
       <ContactSection />
       <OfficeSection />
